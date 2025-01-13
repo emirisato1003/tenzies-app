f7dabec0a133faab636611f86e2bb87443afb1e0
zyam uyan <emirisato.1003@gmail.com>
2025-01-13 11:50:28 -0800

    initial commit

---
+++ src/Die.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Die(props) {
+    const styles = { backgroundColor: props.isHeld ? "#59E391" : "white" };
+    return (
+        <button onClick={props.hold} style={styles}>{props.value}</button>
+    );
+}
